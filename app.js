bfc142ae862a307bd262d7021cfab6926f48571b
¨meldhuicque¨ <mel.dh7@gmail.com>
2021-05-31 16:30:47 -0300

    agregue index con sus estilos e imagenes

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ app.listen(3000, () => {
     console.log('Servidor 3000 corriendo');
 })
 app.get('/' , (req,res) => {
-    res.sendFile(path.resolve(__dirname , './views/home.html'))
+    res.sendFile(path.resolve(__dirname , './views/index.html'))
 });
 app.get('/prodDetail' , (req,res) => {
     res.sendFile(path.resolve(__dirname , './views/prodDetail.html'))
