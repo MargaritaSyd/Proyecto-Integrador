542a11a9a8295e36409c8326c6275db2ec898684
MargaritaSyd <margarita.syddall@gmail.com>
2021-06-16 17:59:46 -0300

    MVC modificado

--- app.js
+++ app.js
@@ -1,29 +1,20 @@
 const express = require ('express');
 const app = express();
 const path = require ('path');
+const indexRoutes = require('./src/routes/indexRoutes');
+const productRoutes = require('./src/routes/productRoutes');
+const userRoutes = require('./src/routes/userRoutes');
 
 app.listen(3000, () => {
     console.log('Servidor 3000 corriendo');
 })
-app.get('/' , (req,res) => {
-    res.sendFile(path.resolve(__dirname , './views/index.html'))
-});
-app.get('/prodetail' , (req,res) => {
-    res.sendFile(path.resolve(__dirname , './views/prodetail.html'))
-});
 
-app.get('/carrito' , (req,res) => {
-    res.sendFile(path.resolve(__dirname , './views/carrito.html'))
-});
-
-app.get('/login' , (req,res) => {
-    res.sendFile(path.resolve(__dirname , './views/login.html'))
-});
-app.get('/signup' , (req,res) => {
-    res.sendFile(path.resolve(__dirname , './views/signup.html'))
-});
- 
 app.use(express.static(path.resolve(__dirname , './public')));
 
+app.set('view engine' , 'ejs');
 
-   
+app.use('/' , indexRoutes);
+
+app.use('/product' , productRoutes);
+
+app.use('/user' , userRoutes);
