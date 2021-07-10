5ba0609608e9ea23542a7c51625b9c7333f9c589
MargaritaSyd <margarita.syddall@gmail.com>
2021-07-09 22:48:32 -0300

    subiendo a heroku

--- app.js
+++ app.js
@@ -6,10 +6,13 @@ const productRoutes = require('./src/routes/productRoutes');
 const userRoutes = require('./src/routes/userRoutes');
 const methodOverride= require('method-override');
 
-app.listen(3000, () => {
-    console.log('Servidor 3000 corriendo');
-})
+// app.listen(3000, () => {
+//     console.log('Servidor 3000 corriendo');
+// })
 
+app.listen(process.env.PORT || 3000, function(){
+    console.log('Servidor corriendo en puerto 3000');
+});
 app.use(express.static(path.resolve(__dirname , './public')));
 
 app.use(methodOverride('_method'));
