84eb1b651bd9f999c3d80af68d780c26a02af41e
lio85 <lionelprats2021@gmail.com>
2021-08-16 00:26:36 -0300

    base de datos subida a always data y conectada con el proyecto y funcionando ok

--- src/dataBase/config/config.js
+++ src/dataBase/config/config.js
@@ -1,26 +1,23 @@
 module.exports = {
   "development": {
-    "username": "root",
-    "password": "37040374",
-    "database": "mameli_version_local",
-    "host": "127.0.0.1",
-    "dialect": "mysql",
-    "port": '3306'
+    "username": "mameli",
+    "password": "maggiemelilio",
+    "database": "mameli_db",
+    "host": "mysql-mameli.alwaysdata.net",
+    "dialect": "mysql"
   },
   "test": {
-    "username": "root",
-    "password": "root",
-    "database": "mameli_version_local",
-    "host": "127.0.0.1",
-    "dialect": "mysql",
-    "port": 3306
+    "username": "mameli",
+    "password": "maggiemelilio",
+    "database": "mameli_db",
+    "host": "mysql-mameli.alwaysdata.net",
+    "dialect": "mysql"
   },
   "production": {
-    "username": "root",
-    "password": "root",
-    "database": "mameli_version_local",
-    "host": "127.0.0.1",
-    "dialect": "mysql",
-    "port": 3306
+    "username": "mameli",
+    "password": "maggiemelilio",
+    "database": "mameli_db",
+    "host": "mysql-mameli.alwaysdata.net",
+    "dialect": "mysql"
   }
 }
