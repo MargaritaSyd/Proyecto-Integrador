8c72f7f23f1970e9fed06af122ee0ac635536f82
lio85 <lionelprats2021@gmail.com>
2021-08-19 22:32:14 -0300

    probando

--- src/dataBase/config/config.js
+++ src/dataBase/config/config.js
@@ -1,10 +1,11 @@
 module.exports = {
   "development": {
-    "username": "mameli",
-    "password": "maggiemelilio",
-    "database": "mameli_db",
-    "host": "mysql-mameli.alwaysdata.net",
-    "dialect": "mysql"
+    "username": "241116",
+    "password": "alwaysdata2021",
+    "database": "lionelprats_testing_proyecto_integrador_lio_testing",
+    "host": "mysql-lionelprats.alwaysdata.net",
+    "dialect": "mysql",
+    "port": 3306
   },
   "test": {
     "username": "mameli",
