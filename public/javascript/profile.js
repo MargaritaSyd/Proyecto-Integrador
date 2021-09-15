991f64abb3e98eb04cdb100cfd0e490ffbd0554d
lio85 <lionelprats2021@gmail.com>
2021-09-15 05:30:46 -0300

    probando fallas

--- public/javascript/profile.js
+++ public/javascript/profile.js
@@ -1,5 +1,13 @@
+let i=0;
 let ingresar= document.querySelector("#ingresar")
-ingresar.addEventListener("click", function(){
-    //alert("Chau")
-    sessionStorage.clear();
+ingresar.addEventListener("click", function(e){
+    console.log(i);
+    while (i==0) {
+        e.preventDefault()
+        sessionStorage.clear();
+        console.log(sessionStorage);
+        i++
+        alert("i vale :"+i+", nos vemos!")
+    } 
+    ingresar.click()    
 })
